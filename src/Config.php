58325ae01bff047dd3c1230c94fc32198c330963
zmeditation <senior.zsoftware@gmail.com>
2021-11-16 00:22:02 -0400

    Fix defaunt DB config DSN port

--- src/Config.php
+++ src/Config.php
@@ -4,5 +4,5 @@ namespace silverorange\DevTest;
 
 class Config
 {
-    public $dsn = 'pgsql:host=localhost;port=5534;dbname=silverorange;user=silverorange;password=silverorange';
+    public $dsn = 'pgsql:host=localhost;port=5532;dbname=silverorange;user=silverorange;password=silverorange';
 }
